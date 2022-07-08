729475168fe803c1b7d0c199da4e2769bf1210d8
Luiz Carlos Cavalcanti <luizcarloscavalcanti@outlook.com>
2022-07-08 16:34:03 -0300

    initial commit

---
+++ app/src/main/java/com/luizcavalcanti/todolist/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.luizcavalcanti.todolist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.luizcavalcanti.todolist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    } 
+}
